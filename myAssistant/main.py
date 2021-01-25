dd447503f626515c182f1a4ed9223965430cf583
von <fengyinpeng@91top.com>
2021-01-25 13:13:49 +0800

    t1

--- myAssistant/main.py
+++ myAssistant/main.py
@@ -1,4 +1,4 @@
-#-*-coding:utf-8-*-
+# -*-coding:utf-8-*-
 
 import sys
 from selenium import webdriver
@@ -6,14 +6,12 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as Expect
 from selenium.webdriver.support.ui import WebDriverWait as Wait
 from selenium.webdriver import FirefoxOptions
-from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 
 # 云麓里销售页面
 url = 'http://www.fangdi.com.cn/new_house/new_house_detail.html?project_id=069c616823d32fc8'
 
 
 def load_driver_path():
-
     platform = sys.platform
 
     if platform.__contains__('darwin'):
@@ -23,38 +21,19 @@ def load_driver_path():
         print('System is linux')
         return 'driver/linux_geckodriver'
 
+
 def dos_can():
     global proxy, server, browser
     try:
 
-        # 获取当前操作系统名称
-
-
-        # 启动代理
-        # server = Server('/Users/von/Documents/exapps/browsermob-proxy-2.1.4/bin/browsermob-proxy')
-        # server.start()
-        # proxy = server.create_proxy()
-        # print('proxy', proxy.proxy)
-
         # 启动浏览器
-
-        profile = webdriver.FirefoxProfile()
-        # profile.set_preference('network.proxy.type', 1)
-        # profile.set_preference('network.proxy.http', '127.0.0.1')
-        # profile.set_preference('network.proxy.http_port', proxy.port)
-        profile.update_preferences()
         options = FirefoxOptions()
         options.add_argument('--headless')
         driver_path = load_driver_path()
         browser = webdriver.Firefox(options=options, executable_path=driver_path)
 
-        # proxy.new_har(options={
-        #     'captureContent': True,
-        #     'captureHeaders': True
-        # })
-
+        # 开始获取内容
         browser.get(url)
-        # time.sleep(10)
 
         Wait(browser, 100).until(
             Expect.presence_of_element_located((By.ID, "salesInformation"))
@@ -62,22 +41,26 @@ def dos_can():
         navBar = browser.find_element_by_id('salesInformation')
         print(str(navBar.text))
         print('-----------------------')
-        nslist = navBar.find_elements_by_class_name('new_house_sale_list')
+        insist = navBar.find_elements_by_class_name('new_house_sale_list')
 
-        csList = nslist[0].find_elements_by_class_name('text_ellipsis')
+        csList = insist[0].find_elements_by_class_name('text_ellipsis')
         print(csList[0].text)
 
-        csList1 = nslist[1].find_elements_by_class_name('text_ellipsis')
+        csList1 = insist[1].find_elements_by_class_name('text_ellipsis')
         print(csList1[0].text)
 
-        csList1 = nslist[3].find_elements_by_class_name('text_ellipsis')
+        csList1 = insist[3].find_elements_by_class_name('text_ellipsis')
         print(csList1[0].text)
 
-        csList1 = nslist[5].find_elements_by_class_name('text_ellipsis')
+        csList1 = insist[5].find_elements_by_class_name('text_ellipsis')
         print(csList1[0].text)
+
+        print('----------------------')
+        salespeed = browser.find_element_by_id('saleSpeed')
+        print(salespeed.text)
+
+
     finally:
-        # proxy.close()
-        # server.stop()
         browser.close()
         browser.quit()
     return ""
